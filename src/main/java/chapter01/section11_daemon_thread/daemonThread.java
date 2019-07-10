726be615e59c2243800c1c92025f2f102150b04d
tony <fengfeng043@gmail.com>
2019-07-10 20:56:20 +0800

    守护线程

---
+++ src/main/java/chapter01/section11_daemon_thread/daemonThread.java
@@ -0,0 +1,35 @@
+package chapter01.section11_daemon_thread;
+
+public class daemonThread {
+	public static void main(String[] args) {
+		try {
+			MyThread thread = new MyThread();
+			thread.setDaemon(true);
+			thread.start();
+			Thread.sleep(5000);
+			System.out.println("我离开thread对象也不再打印了，也就是停止了！");
+		} catch (InterruptedException e) {
+
+			e.printStackTrace();
+		}
+	}
+
+	static class MyThread extends Thread {
+		private int i = 0;
+
+		@Override
+		public void run() {
+			try {
+				while (true) {
+					i++;
+					System.out.println("i=" + (i));
+					Thread.sleep(1000);
+				}
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+	}
+}
